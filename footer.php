f020629b0656136cec6e46c8cd5a2c64d6f1e779
landry0508 <landry5.noel@gmail.com>
2023-11-20 15:28:11 +0100

    footer

---
+++ footer.php
@@ -0,0 +1,55 @@
+<html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="CSS/style.css">
+    </head>
+    <body>
+        
+    </body>
+
+    <footer>
+        <div class="FootContainer">
+            <div class='FooterLeft'>
+                <h4>Notre entreprise</h4>
+                <p>Recrutement</p>
+                <p>Qui sommes nous</p>
+                <p>Engagement durable</p>
+                <p>Nos partenaires</p>
+                <p>Presse et actualité</p>
+            </div>
+
+            <div class='FooterLeft'>
+                <h4>Besoin d'aide ?</h4>
+                <p>Mode de livraison</p>
+                <p>Moyen de paiement</p>
+                <p>Programme de fidélité</p>
+                <p>Tendances sportives</p>
+                <p>Question</p>
+            </div>
+
+            <div class='FooterLeft'>
+                <h4>Nos services</h4>
+                <p>Carte cadeau</p>
+                <p>Louez votre matériel</p>
+                <p>Assurance</p>
+                <p>Financement</p>
+            </div>
+
+            <div class='FooterLeft'>
+                <h4>Nos réseaux</h4>
+                <p>Instagram</p>
+                <p>Twitter</p>
+                <p>Facebook</p>
+                <p>TikTok</p>
+            </div>
+        </div>
+
+        <div>
+            © 202 Dicathlon - Tous droits réservés
+        </div>
+    </footer>
+    </html>
+</html>
